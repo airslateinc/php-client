24db590a27f02f7687069a277daa84561c230c9a
Ivan Nazarov <nazarov.ivan@pdffiller.team>
2019-02-15 18:15:33 +0200

    Added packet-revisions endpoint

---
+++ src/Entities/PacketRevision.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AirSlate\ApiClient\Entities;
+
+class PacketRevision extends BaseEntity
+{
+    /**
+     * @var string
+     */
+    protected $type = 'packet_revisions';
+}
---
+++ src/Services/PacketRevisionsService.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace AirSlate\ApiClient\Services;
+
+use AirSlate\ApiClient\Entities\PacketRevision;
+
+/**
+ * Class PacketsService
+ * @package AirSlate\ApiClient\Services
+ */
+class PacketRevisionsService extends AbstractService
+{
+    public function collection(): array
+    {
+        $url = $this->resolveEndpoint('/packet-revisions');
+
+        $response = $this->httpClient->get($url);
+
+        $content = \GuzzleHttp\json_decode($response->getBody(), true);
+
+        return PacketRevision::createFromCollection($content);
+    }
+}
