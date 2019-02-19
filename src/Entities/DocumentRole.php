f53f19ff4de5cd91525c5f39d5f5968cd61ab3e5
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2019-02-19 17:11:26 +0200

    [ADD-2280] Add document roles endpoint

---
+++ src/Entities/DocumentRole.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AirSlate\ApiClient\Entities;
+
+/**
+ * Class Role
+ * @package AirSlate\ApiClient\Entities
+ *
+ * @property string $id
+ * @property string $title
+ * @property int $required_fields
+ * @property int $fillable_fields
+ */
+
+class DocumentRole extends BaseEntity
+{
+    /**
+     * @var string
+     */
+    protected $type = 'document_roles';
+}
