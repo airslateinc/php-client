4ce51ec658863d40690a6b41bfb935b96d53ff6b
Ievgen Pozdniakov <pozdniakov.ievgen@pdffiller.team>
2019-03-12 11:03:19 +0200

    [AS-593] Update template resource add template relation to slate entity

---
+++ src/Models/Packet/Send/Create.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace AirSlate\ApiClient\Models\Packet\Send;
+
+use AirSlate\ApiClient\Models\ModelInterface;
+
+/**
+ * Class Slate
+ * @package AirSlate\ApiClient\Models
+ *
+ */
+class Create implements ModelInterface
+{
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var string
+     */
+    protected $accessLevel;
+
+    /**
+     * Create PacketSend constructor.
+     *
+     * @param string $email
+     * @param string $accessLevel
+     */
+    public function __construct(string $email, string $accessLevel)
+    {
+        $this->email = $email;
+        $this->accessLevel = $accessLevel;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'type' => 'packet_send',
+            'attributes' => [
+                'email' => $this->email,
+                'access_level' => $this->accessLevel,
+            ],
+        ];
+    }
+}
