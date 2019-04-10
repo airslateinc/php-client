192c69c744574d6a4e6db2f14d6ec5958ae00bf6
volodymyr <yurchenko.volodymyr@pdffiller.team>
2019-04-10 10:42:02 +0300

    ADD-2852 entity properties to dockblock added

--- src/Entities/Tag.php
+++ src/Entities/Tag.php
@@ -5,6 +5,11 @@ namespace AirSlate\ApiClient\Entities;
 /**
  * Class Tag
  * @package AirSlate\ApiClient\Entities
+ *
+ * @property string $id
+ * @property string $name
+ * @property string $created_at
+ * @property string $updated_at
  */
 class Tag extends BaseEntity
 {
