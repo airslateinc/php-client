3af8798e421a7d22bedb60ca43457104e10f7947
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2019-04-16 11:57:18 +0300

    [ADD-2796] Add phpdoc

--- src/Entities/Packets/PacketSigningOrder.php
+++ src/Entities/Packets/PacketSigningOrder.php
@@ -5,6 +5,17 @@ namespace AirSlate\ApiClient\Entities\Packets;
 
 use AirSlate\ApiClient\Entities\BaseEntity;
 
+/**
+ * Class PacketSigningOrder
+ * @package AirSlate\ApiClient\Entities\Packets
+ *
+ * @property string $id
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $order
+ * @property string $role
+ * @property string $status
+ */
 class PacketSigningOrder extends BaseEntity
 {
     /**
