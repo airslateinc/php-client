81e0c4ea46b449da05c4d837fdd9e9ece0b998b3
Ivan <nazarov.ivan@pdffiller.team>
2019-04-26 17:45:00 +0300

    [ADD-2325] Add setInviteEmailAddition

--- src/Models/Packet/Send/Create.php
+++ src/Models/Packet/Send/Create.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace AirSlate\ApiClient\Models\Packet\Send;
 
+use AirSlate\ApiClient\Entities\EntityType;
 use AirSlate\ApiClient\Models\ArrayableInterface;
 
 /**
@@ -12,6 +13,8 @@ use AirSlate\ApiClient\Models\ArrayableInterface;
  */
 class Create implements ArrayableInterface
 {
+    private const INVITE_EMAIL_ADDITION_DEFAULT_ID = 'generic_id';
+
     /**
      * @var string
      */
@@ -21,6 +24,16 @@ class Create implements ArrayableInterface
      * @var string
      */
     protected $accessLevel;
+
+    /**
+     * @var array
+     */
+    private $relationships;
+
+    /**
+     * @var array
+     */
+    private $included;
 
     /**
      * Create PacketSend constructor.
@@ -40,11 +53,37 @@ class Create implements ArrayableInterface
     public function toArray(): array
     {
         return [
-            'type' => 'packet_send',
-            'attributes' => [
-                'email' => $this->email,
-                'access_level' => $this->accessLevel,
+            'data' => [
+                'type' => EntityType::PACKET_SEND,
+                'attributes' => [
+                    'email' => $this->email,
+                    'access_level' => $this->accessLevel,
+                ],
+                'relationships' => $this->relationships
             ],
+            'included' => $this->included
+        ];
+    }
+
+    /**
+     * @param string $subject
+     * @param bool $text
+     * @return void
+     */
+    public function setInviteEmailAddition(string $subject, bool $text): void
+    {
+        $this->relationships[EntityType::INVITE_EMAIL_ADDITION] = [
+            'type' => EntityType::INVITE_EMAIL_ADDITION,
+            'id' => self::INVITE_EMAIL_ADDITION_DEFAULT_ID
+        ];
+
+        $this->included[] = [
+            'id' => self::INVITE_EMAIL_ADDITION_DEFAULT_ID,
+            'type' => EntityType::INVITE_EMAIL_ADDITION,
+            'attributes' => [
+                'subject' => $subject,
+                'text' => $text,
+            ]
         ];
     }
 }
