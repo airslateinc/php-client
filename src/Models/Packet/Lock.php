c6c05c6de1d587b7e8d8697881ddd6e5e1a0e0f4
volodymyr <yurchenko.volodymyr@pdffiller.team>
2019-05-30 16:51:06 +0300

    ADD-3316 code style fixes

--- src/Models/Packet/Lock.php
+++ src/Models/Packet/Lock.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace AirSlate\ApiClient\Models\Packet;
 
+use AirSlate\ApiClient\Exceptions\DomainException;
+
 class Lock
 {
     /**
@@ -16,9 +18,19 @@ class Lock
     public const STATUS_LOCKED = 'LOCKED';
 
     /**
+     * @var array
+     */
+    private const ATTRIBUTES = ['status', 'lock_after_signing_order'];
+
+    /**
      * @var string
      */
     private $status;
+
+    /**
+     * @var bool
+     */
+    private $lock_after_signing_order;
 
     /**
      * @param string $status
@@ -26,7 +38,22 @@ class Lock
      */
     public function setStatus(string $status): self
     {
+        if (!in_array($status, [self::STATUS_LOCKED, self::STATUS_ACTIVE])) {
+            throw new DomainException('Status "' . $status . '" is not allowed');
+        }
+
         $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $lockAfterSigningOrder
+     * @return Lock
+     */
+    public function setLockAfterSigningOrder(bool $lockAfterSigningOrder): self
+    {
+        $this->lock_after_signing_order = $lockAfterSigningOrder;
 
         return $this;
     }
@@ -40,10 +67,28 @@ class Lock
         return [
             'data' => [
                 'type' => 'packets',
-                'attributes' => [
-                    'status' =>  $this->status
-                ]
+                'attributes' => $this->getAttributes()
             ],
         ];
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getAttributes(): array
+    {
+        $attributes = array_reduce(self::ATTRIBUTES, function ($attributes, $property) {
+            if ($this->{$property} !== null) {
+                $attributes[$property] = $this->{$property};
+            }
+
+            return $attributes;
+        }, []);
+
+        if (!count($attributes)) {
+            throw new DomainException('Send attributes are not defined');
+        }
+
+        return $attributes;
     }
 }
