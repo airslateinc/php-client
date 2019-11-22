aef06cf28fc09f40051be35656762452f7bd1ae0
Ivan <nazarov.ivan@pdffiller.team>
2019-11-22 18:18:03 +0200

    [AAD-836] Add get slate invite user

--- src/Entities/SlateInvite.php
+++ src/Entities/SlateInvite.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace AirSlate\ApiClient\Entities;
 
+use AirSlate\ApiClient\Exceptions\MissingDataException;
+use AirSlate\ApiClient\Exceptions\RelationNotExistException;
+use AirSlate\ApiClient\Exceptions\TypeMismatchException;
+
 /**
  * Class SlateInvite
  * @package AirSlate\ApiClient\Entities
@@ -16,5 +20,19 @@ namespace AirSlate\ApiClient\Entities;
  */
 class SlateInvite extends BaseEntity
 {
+    /**
+     * @var string
+     */
     protected $type = EntityType::SLATE_INVITE;
+
+    /**
+     * @return BaseEntity|User|null
+     * @throws RelationNotExistException
+     * @throws MissingDataException
+     * @throws TypeMismatchException
+     */
+    public function getUser(): User
+    {
+        return $this->hasOne(User::class, 'users');
+    }
 }
