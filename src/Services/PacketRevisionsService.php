0c69e2720440bbdc81b5e4456e3a69281861e816
serhii <zashchyk.serhii@pdffiller.team>
2020-05-06 09:38:39 +0300

    [AAD-1370] Added update revision endpoint

--- src/Services/PacketRevisionsService.php
+++ src/Services/PacketRevisionsService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AirSlate\ApiClient\Services;
 
+use AirSlate\ApiClient\Models\Revision\Update;
 use Generator;
 use AirSlate\ApiClient\Entities\PacketRevision;
 use GuzzleHttp\RequestOptions;
@@ -49,6 +50,30 @@ class PacketRevisionsService extends AbstractService
             RequestOptions::QUERY  => [
                 'include' => 'documents'
             ]
+        ]);
+
+        $content = \GuzzleHttp\json_decode($response->getBody(), true);
+
+        return PacketRevision::createFromOne($content);
+    }
+
+    /**
+     * @param string $slateId
+     * @param string $packetId
+     * @param string $revisionId
+     * @param Update $revisionUpdate
+     * @return PacketRevision
+     */
+    public function update(
+        string $slateId,
+        string $packetId,
+        string $revisionId,
+        Update $revisionUpdate
+    ): PacketRevision {
+        $url = $this->resolveEndpoint('flows/' . $slateId . '/packets/' . $packetId . '/revisions/' . $revisionId);
+
+        $response = $this->httpClient->patch($url, [
+            RequestOptions::JSON => $revisionUpdate->toArray()
         ]);
 
         $content = \GuzzleHttp\json_decode($response->getBody(), true);
