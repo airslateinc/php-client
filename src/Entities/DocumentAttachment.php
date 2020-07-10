cc2c369ca0f2634d7b8c4aa7b4cec45c62428ecd
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2020-07-10 14:56:27 +0300

    [AAd-1657] Extend DocumentAttachment with relation

--- src/Entities/DocumentAttachment.php
+++ src/Entities/DocumentAttachment.php
@@ -1,6 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AirSlate\ApiClient\Entities;
+
+use AirSlate\ApiClient\Exceptions\MissingDataException;
+use AirSlate\ApiClient\Exceptions\RelationNotExistException;
+use AirSlate\ApiClient\Exceptions\TypeMismatchException;
 
 /**
  * Class DocumentAttachment
@@ -10,6 +16,7 @@ namespace AirSlate\ApiClient\Entities;
  * @property string $type
  * @property string $subtype
  * @property string $meta
+ * @property-read File|null $file
  */
 class DocumentAttachment extends BaseEntity
 {
@@ -17,4 +24,16 @@ class DocumentAttachment extends BaseEntity
      * @var string
      */
     protected $type = EntityType::DOCUMENT_ATTACHMENT;
+
+    /**
+     * @return File|null
+     * @throws RelationNotExistException
+     * @throws MissingDataException
+     * @throws TypeMismatchException
+     */
+    public function getFile(): ?File
+    {
+        /** @var File|null */
+        return $this->hasOne(File::class, 'file');
+    }
 }
