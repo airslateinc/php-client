9fb0da7c008c6a146c4a332e3a9e2cc75623c55a
Ivan <nazarov.ivan@pdffiller.team>
2020-08-19 14:12:39 +0300

    [AAD-1881] Add contact group

---
+++ src/Entities/RelationshipEntityType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AirSlate\ApiClient\Entities;
+
+interface RelationshipEntityType
+{
+    public const CONTACT_GROUP = 'contact_group';
+    public const INVITE_EMAIL_ADDITION = 'invite_email_addition';
+}
