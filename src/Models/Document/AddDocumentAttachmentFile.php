e2ec5242a6dd25dfabfc6716cefab4f866ae9dd3
Ivan <ivan.nezhura@gmail.com>
2020-08-26 10:57:30 +0300

    HFB-1549 create attachment file endpoint

---
+++ src/Models/Document/AddDocumentAttachmentFile.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace AirSlate\ApiClient\Models\Document;
+
+use AirSlate\ApiClient\Entities\EntityType;
+use AirSlate\ApiClient\Models\AbstractModel;
+
+class AddDocumentAttachmentFile extends AbstractModel
+{
+    /**
+     * @param string $category
+     * @param string $type
+     * @param string $name
+     * @param string $file
+     * @param array $meta
+     */
+    public function addDocumentAttachmentContent(
+        string $category,
+        string $type,
+        string $name,
+        string $file,
+        array $meta = []
+    ) {
+        $this->data = [
+            'type' => EntityType::DOCUMENT_ATTACHMENT,
+            'attributes' => [
+                'type' => $category,
+                'subtype' => $type,
+                'file' => $file,
+                'name' => $name,
+                'meta' => $meta
+            ],
+        ];
+    }
+}
