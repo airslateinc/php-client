d6fb1bcc7e919efdb4e65bbe543276bbf09b1aae
Ivan <nazarov.ivan@pdffiller.team>
2020-09-30 12:35:24 +0300

    [AAD-1385] Add exception code

--- src/Exceptions/ExceptionCode.php
+++ src/Exceptions/ExceptionCode.php
@@ -9,4 +9,5 @@ interface ExceptionCode
     public const FORBIDDEN_CHANGE_PACKET_ROLE_USER_FOR_BOT = 14086;
     public const PAYWALL_SLATES_API = 12111;
     public const PAYWALL_EXPORT_API = 17001;
+    public const INVALID_FIELD_VALUE = 24031;
 }
