39e59fc93998b74a3f0caa9d33f8ce024cdc1e71
Artem Sokolovskyi <sokolovskyi.artem@pdffiller.team>
2020-12-21 12:12:43 +0200

    [AAD-1889] Make search parameters more flexible

--- src/Models/CloudStorage/UpdateOrCreate.php
+++ src/Models/CloudStorage/UpdateOrCreate.php
@@ -53,16 +53,12 @@ class UpdateOrCreate extends AbstractModel
     }
 
     /**
-     * @param string $attribute
-     * @param string[] $inArray
+     * @param array $searchParameter
      * @return $this
      */
-    public function addSearchParameter(string $attribute, array $inArray): self
+    public function addSearchParameter(array $searchParameter): self
     {
-        $this->data['attributes']['search_parameters'][] = [
-            'attribute' => $attribute,
-            'in_array' => $inArray,
-        ];
+        $this->data['attributes']['search_parameters'][] = $searchParameter;
 
         return $this;
     }
