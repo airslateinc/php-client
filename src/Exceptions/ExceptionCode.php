c302dc09464531a0e5815dd8e8a672f19d93342c
volodymyr <yurchenko.volodymyr@pdffiller.team>
2021-03-04 12:36:46 +0200

    AAD-2759 add flow not found code

--- src/Exceptions/ExceptionCode.php
+++ src/Exceptions/ExceptionCode.php
@@ -11,4 +11,5 @@ interface ExceptionCode
     public const PAYWALL_EXPORT_API = 17001;
     public const INVALID_FIELD_VALUE = 24032;
     public const INVALID_PAYLOAD = 0;
+    public const FLOW_NOT_FOUND = 13014;
 }
